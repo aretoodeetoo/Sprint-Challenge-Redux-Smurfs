43519babc177a338f61e3e10ff5ee8018a18357e
croy <roy.christie95@gmail.com>
2019-02-01 10:35:59 -0600

    Added SmurfList component

--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -1,2 +1,15 @@
 import React from 'react';
 
+function SmurfList(props){
+    return(
+        {props.smurfs.map(smurf => (
+            <div className="singleSmurf">
+            <div key={smurf.id}>{smurf.name}</div>
+            <div>{smurf.age}</div>
+            <div>{smurf.height}</div>
+            </div>
+        ))}
+    );
+}
+
+export default SmurfList;
