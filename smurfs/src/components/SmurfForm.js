4d7fb3752fd70979c5a614fcafa53decd2b59c01
croy <roy.christie95@gmail.com>
2019-02-01 11:08:15 -0600

    Created Smurf Form, imported addSmurf props

---
+++ smurfs/src/components/SmurfForm.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function SmurfForm(props){
+    const handleClick = e => {
+        e.preventDefault();
+        props.addNewSmurf();
+    }
+
+    return(
+        <h1>Add A Smurf!</h1>
+        <form>
+            <input 
+            type="text"
+            name="name"
+            placeholder="Name"
+            onChange={props.changeHandler}
+            value={props.smurf.name}
+            />
+            <input 
+            type="text"
+            name="age"
+            placeholder="Age"
+            onChange={props.changeHandler}
+            value={props.smurf.age}
+            />
+            <input 
+            type="text"
+            name="height"
+            placeholder="Height"
+            onChange={props.changeHandler}
+            value={props.smurf.height}
+            />
+
+            <button onClick={handleClick}>Click to Add Smurf</button>
+        </form>
+    );
+}
+
+export default SmurfForm;
