98bfed710738f6d3555efcc55df3393c6651db7d
croy <roy.christie95@gmail.com>
2019-02-01 11:13:27 -0600

    Imported styled components

--- smurfs/src/App.js
+++ smurfs/src/App.js
@@ -13,8 +13,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfListView />
         <SmurfFormView />
+        <SmurfListView />
       </div>
     );
   }
