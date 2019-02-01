5cd52a0689ce228c61bba589d7a8f24bbc4db01f
croy <roy.christie95@gmail.com>
2019-02-01 10:58:02 -0600

    Added an addSmurf action creator

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -4,6 +4,10 @@ export const FETCH_SMURFS_START = 'FETCH_SMURFS_START';
 export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS';
 export const FETCH_SMURFS_FAIL = 'FETCH_SMURFS_FAIL';
 
+export const ADD_SMURFS_START = 'ADD_SMURFS_START';
+export const ADD_SMURFS_SUCCESS = 'ADD_SMURFS_SUCCESS';
+export const ADD_SMURFS_FAIL = 'ADD_SMURFS_FAIL';
+
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -23,4 +27,14 @@ export const getSmurfList = () => dispatch => {
       dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res.data })
       )
     .catch(err => dispatch({ type: FETCH_SMURFS_FAIL, payload: err }))
+}
+
+export const addSmurf = newSmurf => dispatch => {
+  dispatch({ type: ADD_SMURFS_START });
+  axios
+    .post('http://localhost:3333/smurfs', newSmurf)
+    .then(res =>
+      dispatch({ type: ADD_SMURFS_SUCCESS, payload: res.data})
+      )
+    .catch(err => dispatch({ type: ADD_SMURFS_FAIL, payload: err}))
 }
