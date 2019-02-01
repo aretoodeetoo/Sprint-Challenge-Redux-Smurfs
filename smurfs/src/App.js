a5385f5c34b9094a465bc11425a4a9f054fe26cd
croy <roy.christie95@gmail.com>
2019-02-01 12:06:49 -0600

    Added a header and implemented more styling

--- smurfs/src/App.js
+++ smurfs/src/App.js
@@ -12,10 +12,13 @@ import SmurfFormView from './views/SmurfFormView';
 class App extends Component {
   render() {
     return (
+      <div className="header">
+      <h1>Welcome to Smurf Village!</h1>
       <div className="App">
         <SmurfListView />
         <SmurfFormView />
       </div>
+    </div>
     );
   }
 }
