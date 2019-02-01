90d798b3327858ebfbe40ef84cfc457c7d81922a
croy <roy.christie95@gmail.com>
2019-02-01 11:59:57 -0600

    Implemented background images for styling

--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -5,15 +5,15 @@ function SmurfList(props){
     return(
         <div className="smurfWrapper">
         {props.smurfs.map(smurf => (
-            <div  className="singleSmurf">
-            <div>
-            <div key={smurf.id}>{smurf.name}</div>
-            <div>{smurf.age} years old</div>
-            <div>{smurf.height} tall</div>
-            </div>
-            <div className="deleteButton">
-            <img src="https://img.icons8.com/dusk/18/000000/clear-symbol.png" alt="delete" onClick={() => props.deleteSmurf(smurf.id)}/>
-            </div>
+            <div className="singleSmurf">
+                <div className="smurfData">
+                    <div key={smurf.id}>{smurf.name}</div>
+                    <div>{smurf.age} years old</div>
+                    <div>{smurf.height} tall</div>
+                </div>
+                <div className="deleteButton">
+                    <img src="https://img.icons8.com/dusk/24/000000/clear-symbol.png" alt="delete" onClick={() => props.deleteSmurf(smurf.id)}/>
+                </div>
             </div>
         ))}
         </div>
