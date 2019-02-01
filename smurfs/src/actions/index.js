dc1f8d8f6bf9e95765b5be4b75a2d57778621baa
croy <roy.christie95@gmail.com>
2019-02-01 10:39:52 -0600

    Added action creator to get the initial smurf list

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,6 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+export const FETCH_SMURFS_START = 'FETCH_SMURFS_START';
+export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS';
+export const FETCH_SMURFS_FAIL = 'FETCH_SMURFS_FAIL';
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +12,13 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const getSmurfList = () => dispatch => {
+  dispatch ({ type: FETCH_SMURFS_START });
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(res => 
+      dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res.data })
+      )
+    .catch(err => dispatch({ type: FETCH_SMURFS_FAIL, payload: err }))
+}
